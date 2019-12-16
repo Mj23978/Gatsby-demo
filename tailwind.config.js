7383fbca4ca27ea2661bb9d22f4c196e0fcc51de
Mj23978 <johndoe2561357@protonmail.com>
2019-12-16 04:58:04 +0330

    #2 Configure Starter

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          lighter: 'rgba(43, 142, 222, 1)',
+          default: 'rgba(65, 153, 225, 1)',
+          darker: 'rgba(30, 120, 194, 1)'
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
