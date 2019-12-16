12c4fc4c0cadd9aff1cb2e7f099ab1aec5529e78
Mj23978 <johndoe2561357@protonmail.com>
2019-12-16 05:27:00 +0330

    #3 Edit package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  plugins: []
+  plugins: [
+    {
+      resolve: `gatsby-theme-codebushi`,
+      options: {
+        tailwindConfig: `tailwind.config.js`
+      }
+    }
+  ]
 }
