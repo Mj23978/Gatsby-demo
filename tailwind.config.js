36a45018433e83db173246a4c896fcae389e01c9
Mj23978 <45851576+Mj23978@users.noreply.github.com>
2020-08-03 23:04:36 +0430

    change site

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          lighter: 'rgba(43, 142, 222, 1)',
-          default: 'rgba(65, 153, 225, 1)',
-          darker: 'rgba(30, 120, 194, 1)'
+          lighter: 'hsl(207, 73%, 52%)',
+          default: 'hsl(207, 73%, 57%)',
+          darker: 'hsl(207, 73%, 44%)'
         }
       }
     }
   },
   variants: {},
   plugins: []
-}
+};
